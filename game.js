0cde5f095ced6e8b2218143c0f2b90b688e18eca
MalikAsad01 <90966494+MalikAsad01@users.noreply.github.com>
2021-10-30 16:40:06 +0400

    Update game.js

--- game.js
+++ game.js
@@ -1,33 +1,30 @@
 function Bear() {
-this.dBear = 100;
-this.htmlElement = document.getElementById("bear");
-this.id = this.htmlElement.id;
-this.x = this.htmlElement.offsetLeft; 
-this.y = this.htmlElement.offsetTop;
-  
-this.move = function(xDir, yDir) { 
-  this.x += this.dBear * xDir; 
-  this.y += this.dBear * yDir;
-  this.display();
- };
+  this.dBear = 100;
+  this.htmlElement = document.getElementById("bear");
+  this.id = this.htmlElement.id;
+  this.x = this.htmlElement.offsetLeft; 
+  this.y = this.htmlElement.offsetTop;
+  this.move = function(xDir, yDir) { 
+    this.x += this.dBear * xDir; 
+    this.y += this.dBear * yDir;
+    this.display();
+  };
 
-this.display = function() {
-  this.htmlElement.style.left = this.x + "px";
-  this.htmlElement.style.top = this.y + "px";
-  this.htmlElement.style.display = "absolute";
- };
-this.fitBounds = function() {
-  let parent = this.htmlElement.parentElement; let iw = this.htmlElement.offsetWidth;
-  let ih = this.htmlElement.offsetHeight; let l = parent.offsetLeft;
-  let t = parent.offsetTop;
-  let w = parent.offsetWidth; let h = parent.offsetHeight; if (this.x < 0) this.x = 0;
-  if (this.x > w - iw) this.x = w - iw;
-  if (this.y < 0) this.y = 0;
-  if (this.y > h - ih) this.y = h - ih;
- 
- };
+  this.display = function() {
+    this.htmlElement.style.left = this.x + "px";
+    this.htmlElement.style.top = this.y + "px";
+    this.htmlElement.style.display = "absolute";
+  };
+  this.fitBounds = function() {
+    let parent = this.htmlElement.parentElement; let iw = this.htmlElement.offsetWidth;
+    let ih = this.htmlElement.offsetHeight; let l = parent.offsetLeft;
+    let t = parent.offsetTop;
+    let w = parent.offsetWidth; let h = parent.offsetHeight; if (this.x < 0) this.x = 0;
+    if (this.x > w - iw) this.x = w - iw;
+    if (this.y < 0) this.y = 0;
+    if (this.y > h - ih) this.y = h - ih;
+  };
   
-
 }
 
 function start() {
