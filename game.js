7412db7bfe27829bf610369f18ec101c3ae155ad
MalikAsad01 <90966494+MalikAsad01@users.noreply.github.com>
2021-10-30 16:37:03 +0400

    Update game.js

--- game.js
+++ game.js
@@ -16,6 +16,17 @@ this.display = function() {
   this.htmlElement.style.top = this.y + "px";
   this.htmlElement.style.display = "absolute";
  };
+this.fitBounds = function() {
+  let parent = this.htmlElement.parentElement; let iw = this.htmlElement.offsetWidth;
+  let ih = this.htmlElement.offsetHeight; let l = parent.offsetLeft;
+  let t = parent.offsetTop;
+  let w = parent.offsetWidth; let h = parent.offsetHeight; if (this.x < 0) this.x = 0;
+  if (this.x > w - iw) this.x = w - iw;
+  if (this.y < 0) this.y = 0;
+  if (this.y > h - ih) this.y = h - ih;
+ 
+ };
+  
 
 }
 
