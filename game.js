f13827010be0e483a7a9a135e8f9ab55bb22bb92
MalikAsad01 <90966494+MalikAsad01@users.noreply.github.com>
2021-10-31 22:31:52 +0400

    made changes made changes

--- game.js
+++ game.js
@@ -1,3 +1,8 @@
+var bear;
+var bees;
+var updateTimer;
+var lastStingTime;
+
 function Bear() {
   this.dBear = 100;
   this.htmlElement = document.getElementById("bear");
@@ -43,17 +48,17 @@ function moveBear(e) {
   const KEYLEFT = 37;
   const KEYRIGHT = 39;
 
-  if (e.keyCode == KEYRIGHT) {
+  if (e.keyCode === KEYRIGHT) {
     bear.move(1, 0);
   } // right key
-  if (e.keyCode == KEYLEFT) {
+  if (e.keyCode === KEYLEFT) {
     bear.move(-1, 0);
   } // left key
 
-  if (e.keyCode == KEYUP) {
+  if (e.keyCode === KEYUP) {
     bear.move(0, -1);
   } // up key
-  if (e.keyCode == KEYDOWN) {
+  if (e.keyCode === KEYDOWN) {
     bear.move(0, 1);
   } // down key
 }
@@ -85,7 +90,8 @@ class Bee {
     };
 
     this.fitBounds = function () {
-      //check and make sure the bees stays in the board space let parent = this.htmlElement.parentElement;
+      //check and make sure the bees stays in the board space
+      let parent = this.htmlElement.parentElement;
       let iw = this.htmlElement.offsetWidth;
       let ih = this.htmlElement.offsetHeight;
       let l = parent.offsetLeft;
@@ -145,6 +151,74 @@ function makeBees() {
   }
 }
 
+function moveBees() {
+  //get speed input field value
+  let speed = document.getElementById("speedBees").value;
+  //move each bee to a random location
+  for (let i = 0; i < bees.length; i++) {
+    let dx = getRandomInt(2 * speed) - speed;
+    let dy = getRandomInt(2 * speed) - speed;
+    bees[i].move(dx, dy);
+    isHit(bees[i], bear); //we add this to count stings
+  }
+}
+
+function updateBees() {
+  // update loop for game
+  //move the bees randomly
+  moveBees();
+  //use a fixed update period
+  let period = 1000; //modify this to control refresh period
+  //update the timer for the next move
+  updateTimer = setTimeout("updateBees()", period);
+}
+
+function overlap(element1, element2) {
+  //consider the two rectangles wrapping the two elements
+  //rectangle of the first element
+  left1 = element1.htmlElement.offsetLeft;
+  top1 = element1.htmlElement.offsetTop;
+  right1 = element1.htmlElement.offsetLeft + element1.htmlElement.offsetWidth;
+  bottom1 = element1.htmlElement.offsetTop + element1.htmlElement.offsetHeight;
+
+  //rectangle of the second element
+  left2 = element2.htmlElement.offsetLeft; //e2x
+  top2 = element2.htmlElement.offsetTop; //e2y
+  right2 = element2.htmlElement.offsetLeft + element2.htmlElement.offsetWidth;
+  bottom2 = element2.htmlElement.offsetTop + element2.htmlElement.offsetHeight;
+
+  //calculate the intersection of the two rectangles
+  x_intersect = Math.max(0, Math.min(right1, right2) - Math.max(left1, left2));
+  y_intersect = Math.max(0, Math.min(bottom1, bottom2) - Math.max(top1, top2));
+  intersectArea = x_intersect * y_intersect;
+
+  //if intersection is nil no hit
+  if (intersectArea == 0 || isNaN(intersectArea)) {
+    return false;
+  }
+  return true;
+}
+
+function isHit(defender, offender) {
+  if (overlap(defender, offender)) {
+    //check if the two image overlap
+    let score = hits.innerHTML;
+    score = Number(score) + 1; //increment the score
+    hits.innerHTML = score; //display the new score
+    //calculate longest duration
+    let newStingTime = new Date();
+    let thisDuration = newStingTime - lastStingTime;
+    lastStingTime = newStingTime;
+    let longestDuration = Number(duration.innerHTML);
+    if (longestDuration === 0) {
+      longestDuration = thisDuration;
+    } else {
+      if (longestDuration < thisDuration) longestDuration = thisDuration;
+    }
+    document.getElementById("duration").innerHTML = longestDuration;
+  }
+}
+
 function start() {
   //create bear
   bear = new Bear();
@@ -152,6 +226,9 @@ function start() {
   document.addEventListener("keydown", moveBear, false);
   //create new array for bees
   bees = new Array();
-  //create bees makeBees();
+  //create bees
   makeBees();
+  updateBees();
+  //take start time
+  lastStingTime = new Date();
 }
